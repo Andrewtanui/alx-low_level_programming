2d5c3d7ad847422a3bf47e108c73bef011602330
Andrewtanui <126679653+Andrewtanui@users.noreply.github.com>
2023-04-11 10:13:32 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,12 +8,13 @@
  */
 void print_alphabet(void)
 {
-	char letter = 'a';
+	char c = 'a';
 
-	while (letter <= 'z')
+	while (c <= 'z')
 	{
-		_putchar(letter);
-		letter++;
+		_putchar(c);
+		c++;
 	}
 	_putchar('\n');
 }
+
