faa71da9bf2d3c9bb2f31c3027235400f2570000
Andrewtanui <126679653+Andrewtanui@users.noreply.github.com>
2023-04-12 00:20:16 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *array;
-	unsigned int index;
+	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
@@ -23,8 +23,8 @@ char *create_array(unsigned int size, char c)
 	if (array == NULL)
 		return (NULL);
 
-	for (index = 0; index < size; index++)
-		array[index] = c;
+	for (i = 0; i < size; i++)
+		array[i] = c;
 
 	return (array);
 }
