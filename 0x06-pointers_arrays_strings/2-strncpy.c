861125f8b443173856a377ff5b80578e928006fe
Andrewtanui <126679653+Andrewtanui@users.noreply.github.com>
2023-04-13 10:34:58 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,26 +1,34 @@
-#include "main.h"
-
 /**
- * _strncpy - Copies at most an inputted number
- *            of bytes from string src into dest.
- * @dest: The buffer storing the string copy.
- * @src: The source string.
- * @n: The maximum number of bytes to copied from src.
+ * _strncpy - Copies up to n characters from a string
  *
- * Return: A pointer to the resulting string dest.
+ * @dest: Pointer to the destination string
+ * @src: Pointer to the source string
+ * @n: Maximum number of characters to copy
+ *
+ * Return: Pointer to the resulting string dest
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0, src_len = 0;
+        int index = 0, src_len = 0;
 
-	while (src[index++])
-		src_len++;
+        /* Find length of src string */
+        while (src[index++])
+        {
+                src_len++;
+        }
 
-	for (index = 0; src[index] && index < n; index++)
-		dest[index] = src[index];
+        /* Copy up to n characters from src to dest */
+        for (index = 0; src[index] && index < n; index++)
+        {
+                dest[index] = src[index];
+        }
 
-	for (index = src_len; index < n; index++)
-		dest[index] = '\0';
+        /* Fill remaining space in dest with null terminators */
+        for (index = src_len; index < n; index++)
+        {
+                dest[index] = '\0';
+        }
 
-	return (dest);
+        return (dest);
 }
