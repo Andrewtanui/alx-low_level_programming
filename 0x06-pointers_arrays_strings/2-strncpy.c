c7719611faf9e9974ec89064e42da30c5857c32e
Andrewtanui <126679653+Andrewtanui@users.noreply.github.com>
2023-04-13 10:42:45 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,25 +11,17 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-        int index = 0, src_len = 0;
+	int index = 0, src_len = 0;
 
-        /* Find length of src string */
-        while (src[index++])
-        {
-                src_len++;
-        }
+	while (src[index++])
+		src_len++;
 
-        /* Copy up to n characters from src to dest */
-        for (index = 0; src[index] && index < n; index++)
-        {
-                dest[index] = src[index];
-        }
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
 
-        /* Fill remaining space in dest with null terminators */
-        for (index = src_len; index < n; index++)
-        {
-                dest[index] = '\0';
-        }
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
 
-        return (dest);
+	return (dest);
 }
+
