369dc7ff71c2f5b05adefe1970efbfeaca597514
Andrewtanui <lelanandre9@gmail.com>
2023-04-17 07:03:39 -0700

    Task 0

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef OBJECT_LIKE_MACRO_H  
+#define OBJECT_LIKE_MACRO_H  
+
+/*
+ * This header file defines a macro named SIZE, which is used to represent
+ * a fixed size or capacity in various data structures or algorithms.
+ */
+
+#define SIZE 1024 
+
+#endif 
