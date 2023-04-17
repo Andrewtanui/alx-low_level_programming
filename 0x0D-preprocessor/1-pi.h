a75456b24b96f40c19fe44927c228b8f8d17bd27
Andrewtanui <126679653+Andrewtanui@users.noreply.github.com>
2023-04-17 21:51:02 +0300

    Update 1-pi.h

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -3,10 +3,10 @@
 
 /*
  * File: 1-pi.h
- * Desc: Header file that defines a macro named PI
+ * Description: Header file that defines a macro named PI
  *       as an abbreviation for the token 3.14159265359.
  */
 
 #define PI 3.14159265359
 
-#endif 
+#endif
