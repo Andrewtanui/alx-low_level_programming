721d2c5d771ad885015e3141151b70c9c5f1a251
Andrewtanui <lelanandre9@gmail.com>
2023-04-17 11:47:04 -0700

    Task 1

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,12 @@
+#ifndef MY_HEADER_FILE_H
+#define MY_HEADER_FILE_H
+
+/*
+ * File: 1-pi.h
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif 
