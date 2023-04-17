871dcce48ec827c5d5c5d940105215a51853ccf8
Andrewtanui <126679653+Andrewtanui@users.noreply.github.com>
2023-04-17 22:00:17 +0300

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point of the program. 
- *This program prints the name of the file it was compiled from.
+ * This program prints the name of the file it was compiled from.
  *
  * Return: Always 0 (success)
  */
