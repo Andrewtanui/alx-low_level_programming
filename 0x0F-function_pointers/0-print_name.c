324cd3dcea647269194fab6acab9396959f0db0b
Andrewtanui <lelanandre9@gmail.com>
2023-04-19 13:05:44 -0700

    Task 1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,29 +1,15 @@
-#include <stdio.h>
 
-void print_name(char *name, void (*f)(char *)) {
-  f(name);
-}
+#include "function_pointers.h"
 
-void print_normal(char *name) {
-  printf("Name: %s\n", name);
-}
+/**
+ * print_name - Prints name.
+ * @name: The name that is to be printed.
+ * @f: A pointer to a function that prints name.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
 
-void print_reversed(char *name) {
-  printf("Reversed name: ");
-  for (int i = strlen(name) - 1; i >= 0; i--) {
-    printf("%c", name[i]);
-  }
-  printf("\n");
-}
-
-int main() {
-  char *name = "John Doe";
-
-  printf("Printing name normally:\n");
-  print_name(name, print_normal);
-
-  printf("Printing name reversed:\n");
-  print_name(name, print_reversed);
-
-  return 0;
+	f(name);
 }
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/*
+ * desc: Header file that contains prototypes for all functions
+ * 	 used in 0x0F-function_pointers directory
+ */
+
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
