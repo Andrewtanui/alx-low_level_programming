d4270bb1d20edcd00a062ba8f4c2a40b9579ad66
Andrewtanui <126679653+Andrewtanui@users.noreply.github.com>
2023-04-29 11:22:01 +0300

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -6,10 +6,14 @@ section .text
     extern printf
 
 main:
-    sub rsp, 8          ; Align stack to 16-byte boundary
+    push rbp            ; Save rbp on the stack
+    mov rbp, rsp        ; Set rbp to current rsp value
+    sub rsp, 16         ; Allocate 16 bytes of space on the stack
     mov rdi, msg        ; Set first argument (format string) to msg
     xor esi, esi        ; Set second argument (varargs) to 0
     mov eax, 0          ; Set return value to 0
     call printf         ; Call printf to print the message
-    add rsp, 8          ; Restore stack alignment
+    add rsp, 16         ; Deallocate the 16 bytes of space on the stack
+    mov rsp, rbp        ; Restore rsp to its original value
+    pop rbp             ; Restore rbp from the stack
     ret                 ; Return from main
