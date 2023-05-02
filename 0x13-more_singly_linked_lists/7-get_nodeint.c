35e4e79a3afc3756e8314325c42bea449573f8f9
Andrewtanui <126679653+Andrewtanui@users.noreply.github.com>
2023-05-02 09:08:46 +0300

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,14 +12,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
     unsigned int i;
 
-    for (i = 0; head && i < index; i++)
-    {
-    
-    	if (head == NULL)
-		return (NULL);
-		
+    for (i = 0; i < index; i++)
         head = head->next;
-     }
 
-    return (head);
+    return (i == index ? head : NULL);
 }
