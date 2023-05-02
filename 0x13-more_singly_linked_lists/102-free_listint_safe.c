1854cc2fed94a658ba1f076843e1990025815179
Andrewtanui <126679653+Andrewtanui@users.noreply.github.com>
2023-05-02 12:33:12 +0300

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,40 +1,37 @@
-#include "lists.h"
-
-size_t looped_listint_count(listint_t *head);
-size_t free_listint_safe(listint_t **h);
-
 /**
- * free_listint_safe - Frees a listint_t list
- * @h: Double pointer to the head of the list
+ * free_listint_safe - Frees a listint_t linked list safely.
+ * @h: A pointer to the head of the linked list.
  *
- * Return: The size of the list that was freed
+ * Return: The number of nodes in the list that were freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current, *tmp;
-	size_t count = 0;
+    size_t count = 0;
+    listint_t *current, *tmp;
+    int diff;
 
-	if (h == NULL)
-		return (0);
+    if (h == NULL || *h == NULL)
+        return (count);
 
-	current = *h;
+    current = *h;
+    while (current != NULL)
+    {
+        diff = current - current->next;
+        if (diff > 0)
+        {
+            tmp = current;
+            current = current->next;
+            free(tmp);
+            count++;
+        }
+        else
+        {
+            free(current);
+            count++;
+            *h = NULL;
+            break;
+        }
+    }
 
-	while (current != NULL)
-	{
-		count++;
-		if (current < current->next)
-		{
-			tmp = current;
-			current = current->next;
-			free(tmp);
-		}
-		else
-		{
-			free(current);
-			*h = NULL;
-			break;
-		}
-	}
-
-	return (count);
+    return (count);
 }
