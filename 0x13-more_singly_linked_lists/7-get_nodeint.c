46749632f58ed62ad25f28c9992e0c0f7d85262a
Andrewtanui <126679653+Andrewtanui@users.noreply.github.com>
2023-05-02 09:18:27 +0300

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,25 +1,20 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - Returns the nth node of a listint_t linked list.
- * @head: Pointer to the head of the linked list.
- * @index: Index of the node to return.
+ * sum_listint - returns the sum of all the data (n) of a listint_t linked list.
+ * @head: pointer to the first node of the list
  *
- * Return: If the node exists, return a pointer to the node.
- *         Otherwise, return NULL.
+ * Return: sum of all the data, or 0 if list is empty
  */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+int sum_listint(listint_t *head)
 {
-    unsigned int count = 0;
-    listint_t *current = head;
+    int sum = 0;
 
-    while (current != NULL)
+    while (head != NULL)
     {
-        if (count == index)
-            return (current);
-        count++;
-        current = current->next;
+        sum += head->n;
+        head = head->next;
     }
 
-    return (NULL);
+    return (sum);
 }
