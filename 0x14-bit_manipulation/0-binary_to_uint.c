8b5323053480220d5aadc58ff873d060be9b3c99
Andrewtanui <126679653+Andrewtanui@users.noreply.github.com>
2023-05-04 18:41:05 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,7 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
+	unsigned int num;
 	int i;
 
 	if (!b)
@@ -17,14 +17,13 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] == '0')
+			num = num << 1;
+		else if (b[i] == '1')
+			num = (num << 1) | 1;
+		else
 			return (0);
 	}
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		num <<= 1;
-		if (b[i] == '1')
-			num += 1;
-	}
+
 	return (num);
 }
