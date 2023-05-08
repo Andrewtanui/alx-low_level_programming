2873b2ad967d281fa756f90c88bdb8eebbad3305
Andrewtanui <126679653+Andrewtanui@users.noreply.github.com>
2023-05-08 13:42:53 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,5 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-<<<<<<< HEAD
-#include <fcntl.h>
-#include <unistd.h>
 
 /**
  * read_textfile - Reads a text file and prints it to POSIX stdout.
@@ -54,45 +51,4 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	close(file_descriptor);
 
 	return (bytes_written);
-}
-=======
-#include "main.h"
-
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-    if (filename == NULL)
-        return 0;
-
-    FILE *file = fopen(filename, "r");
-    if (file == NULL)
-        return 0;
-
-    char *buffer = (char *)malloc(sizeof(char) * (letters + 1));
-    if (buffer == NULL)
-    {
-        fclose(file);
-        return 0;
-    }
-
-    ssize_t bytesRead = fread(buffer, sizeof(char), letters, file);
-    if (bytesRead < 0)
-    {
-        free(buffer);
-        fclose(file);
-        return 0;
-    }
-
-    ssize_t bytesWritten = fwrite(buffer, sizeof(char), bytesRead, stdout);
-    if (bytesWritten != bytesRead)
-    {
-        free(buffer);
-        fclose(file);
-        return 0;
-    }
-
-    free(buffer);
-    fclose(file);
->>>>>>> 3e663cdcd45a424928c0694002a2703c04aa71c7
-
-    return bytesRead;
 }
