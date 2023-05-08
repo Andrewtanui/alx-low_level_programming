a1b04ebdacbf634191b87d7a22998859408aadbe
Andrewtanui <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@gmail.com>
2023-05-08 03:14:47 -0700

    Header File

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-/*
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x14-file_io directory.
- */
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -16,4 +11,5 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif 
+#endif /* MAIN_H */
+
